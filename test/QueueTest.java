b05c2091f14538ce2d05038aa26840e853bf71b5
Luis <lfponton@gmail.com>
2021-03-30 10:10:22 +0200

    3 tests added

--- test/QueueTest.java
+++ test/QueueTest.java
@@ -1,16 +1,48 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QueueTest
 {
+
+  private QueueADT<String> q;
+
+  @BeforeEach
+  public void createQueue() {
+    // arrange
+    q = new BoundedArrayQueue<>(5);
+  }
   @Test
   public void addedElementIsInQueue() {
-    // arrange
-    QueueADT<String> q = new BoundedArrayQueue<>(5);
     // act
     q.enqueue("A");
     // assert
     assertTrue(q.contains("A"));
+  }
+
+  @Test
+  public void addMultipleElementsToQueue() {
+    // act
+    q.enqueue("A");
+    q.enqueue("B");
+    q.enqueue("C");
+    q.enqueue("D");
+    // assert
+    assertTrue(q.contains("A"));
+    assertTrue(q.contains("B"));
+    assertTrue(q.contains("C"));
+    assertTrue(q.contains("D"));
+  }
+
+  @Test
+  public void sizeIs4AfterAdding4Elements() {
+    // act
+    q.enqueue("A");
+    q.enqueue("B");
+    q.enqueue("C");
+    q.enqueue("D");
+    // assert
+    assertEquals(4, q.size());
   }
 }
